1befd10cdd85b2cd5817c330c755ceea993a340f
undeadgorilla <dennis.marx93@gmail.com>
2024-03-27 10:37:25 +0100

    update listing

--- public/index.php
+++ public/index.php
@@ -4,13 +4,6 @@ require '../helpers.php';
 
 use Framework\Router;
 
-// spl_autoload_register(function ($class) {
-//   $path = basePath('Framework/' . $class . '.php');
-//   if (file_exists($path)) {
-//     require $path;
-//   }
-// });
-
 // Instantiating the router
 $router = new Router();
 
